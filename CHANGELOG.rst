bf8ec46c672ed5a7ba2471d9b514cef15995bf3f
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2022-03-07 19:44:16 +0530

    Added change log and set release version

---
+++ CHANGELOG.rst
@@ -0,0 +1,6 @@
+1.0.0 - Mar 07, 2022. (sha 2e082a4ec0868e03df1b902562810873421823e5)
+--------------------------------------------------------------------
+
+* Initial PyPi release of total-perspective-vortex.
+* Basic usage docs and examples.
+* 94% test coverage.
--- vortex/__init__.py
+++ vortex/__init__.py
@@ -1,7 +1,7 @@
 """Total Perspective Vortex library setup."""
 
 # Current version of the library
-__version__ = "0.1.0+dev"
+__version__ = "1.0.0"
 
 
 def get_version():
@@ -9,6 +9,6 @@ def get_version():
     Return a string with the current version of the library.
 
     :rtype: ``string``
-    :return:  Library version (e.g., "0.1.0").
+    :return:  Library version (e.g., "1.0.0").
     """
     return __version__
