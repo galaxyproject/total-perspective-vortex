26092be1c617c639d5285cb0aa9b6ef5fbe97469
cat-bro <cathjb@gmail.com>
2022-08-09 10:46:32 +1000

    Add context tests for context variables accessed within rules

--- tests/test_mapper_context.py
+++ tests/test_mapper_context.py
@@ -37,3 +37,21 @@ class TestMapperContext(unittest.TestCase):
         self.assertEqual(destination.id, "k8s_environment")
         self.assertEqual([env['value'] for env in destination.env if env['name'] == 'TEST_JOB_SLOTS'], ['5'])
         self.assertEqual(destination.params['native_spec'], '--mem 15 --cores 5 --gpus 4')
+
+    def test_context_variable_overridden_in_rule(self):
+        # test that job will not fail with 40GB input size because large_input_size has been set to 60
+        tool = mock_galaxy.Tool('bwa')
+        user = mock_galaxy.User('gargravarr', 'fairycake@vortex.org')
+        datasets = [mock_galaxy.DatasetAssociation("test", mock_galaxy.Dataset("test.txt", file_size=40*1024**3))]
+
+        destination = self._map_to_destination(tool, user, datasets)
+        self.assertEqual(destination.params['native_spec'], '--mem 15 --cores 5 --gpus 4')
+
+    def test_context_variable_defined_for_tool_in_rule(self):
+        # test that context variable set for tool entity but not set in ancestor entities is defined
+        tool = mock_galaxy.Tool('canu')
+        user = mock_galaxy.User('gargravarr', 'fairycake@vortex.org')
+        datasets = [mock_galaxy.DatasetAssociation("test", mock_galaxy.Dataset("test.txt", file_size=5*1024**3))]
+
+        destination = self._map_to_destination(tool, user, datasets)
+        self.assertEqual(destination.params['native_spec'], '--mem 9 --cores 3 --gpus 1')
