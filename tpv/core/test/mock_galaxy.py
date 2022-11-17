87da3b98acc17808a5c2a77b62c7c974eb55ccc9
Nate Coraor <nate@bx.psu.edu>
2022-11-17 13:01:24 -0500

    Update mock default job_conf for relocation

--- tpv/core/test/mock_galaxy.py
+++ tpv/core/test/mock_galaxy.py
@@ -54,9 +54,9 @@ class Tool:
 
 # App mock=======================================================
 class App:
-    def __init__(self, job_conf='fixtures/job_conf.yml', create_model=False):
+    def __init__(self, job_conf=None, create_model=False):
         self.config = bunch.Bunch(
-            job_config_file=os.path.join(os.path.dirname(__file__), job_conf),
+            job_config_file=job_conf,
             use_tasked_jobs=False,
             job_resource_params_file="/tmp/fake_absent_path",
             config_dict={},
