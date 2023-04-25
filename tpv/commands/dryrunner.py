c2f28b9596d12c2d15cc953b3564a83c069d1dd2
cat-bro <cathjb@gmail.com>
2023-04-26 02:08:36 +1000

    Add tool version to dry-run

--- tpv/commands/dryrunner.py
+++ tpv/commands/dryrunner.py
@@ -29,11 +29,10 @@ class TPVDryRunner():
             user = None
 
         if tool:
-            tool = mock_galaxy.Tool(tool)
-            # tool = mock_galaxy.Tool(
-            #     tool,
-            #     version=tool.split('/')[-1] if '/' in tool else '0._unversioned'
-            # )
+            tool = mock_galaxy.Tool(
+                tool,
+                version=tool.split('/')[-1] if '/' in tool else None
+            )
         else:
             tool = None
 
