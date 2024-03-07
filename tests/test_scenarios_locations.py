56f4059bee8f40451c5afbdd735c3fc6e7b514f9
pauldg <pauldegeest1@gmail.com>
2024-03-07 11:57:22 +0100

    add queue size to test

--- tests/test_scenarios_locations.py
+++ tests/test_scenarios_locations.py
@@ -49,6 +49,40 @@ class TestScenarios(unittest.TestCase):
         Each endpoint will have a user that does this.
         """
 
+        def create_job(app, destination):
+            sa_session = app.model.context
+
+            u = app.model.User(email="highmemuser@unimelb.edu.au", password="password")
+            j = app.model.Job()
+            j.user = u
+            j.tool_id = "trinity"
+            j.state = "running"
+            j.destination_id = destination
+
+            sa_session.add(j)
+            sa_session.flush()
+
+        def create_job_state_history(app, destination):
+            sa_session = app.model.context
+
+            u = app.model.User(email="highmemuser@unimelb.edu.au", password="password")
+            j = app.model.Job()
+            j.user = u
+            j.tool_id = "trinity"
+            j.state = state
+            j.destination_id = destination
+
+            sa_session.add(j)
+            sa_session.flush()
+
+        app = mock_galaxy.App(
+            job_conf=os.path.join(os.path.dirname(__file__), 'fixtures/job_conf_scenario_locations.yml'),
+            create_model=True)
+        create_job(app, "highmem_pulsar_1")
+        create_job(app, "highmem_pulsar_2")
+        create_job(app, "highmem_pulsar_1")
+        create_job(app, "highmem_pulsar_2")
+
         tool = mock_galaxy.Tool('trinity')
         user = mock_galaxy.User('pulsar-hm2-user', 'pulsar-hm2-user@unimelb.edu.au', roles=["ga_admins"])
         datasets = [mock_galaxy.DatasetAssociation("input", mock_galaxy.Dataset("input.fastq",
@@ -56,6 +90,6 @@ class TestScenarios(unittest.TestCase):
                                                                                 object_store_id="object_store_australia"))]
         rules_file = os.path.join(os.path.dirname(__file__), 'fixtures/scenario-locations-api.yml')
         destination = self._map_to_destination(tool, user, datasets=datasets, tpv_config_path=rules_file,
-                                                job_conf='fixtures/job_conf_scenario_locations.yml')
+                                                job_conf='fixtures/job_conf_scenario_locations.yml', app=app)
         self.assertEqual(destination.id, "pulsar_australia")
 
