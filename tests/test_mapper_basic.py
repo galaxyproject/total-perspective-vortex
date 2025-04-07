610ef3c31606737cb8a40b968a4fda3811ba413d
nuwang <2070605+nuwang@users.noreply.github.com>
2025-04-07 18:11:54 +0530

    Also add tests for absent tpv configs

--- tests/test_mapper_basic.py
+++ tests/test_mapper_basic.py
@@ -4,26 +4,28 @@ import unittest
 
 from galaxy.jobs.mapper import JobMappingException
 
-from tpv.core.util import load_yaml_from_url_or_path
 from tpv.commands.test import mock_galaxy
-from tpv.rules import gateway
+from tpv.core.util import load_yaml_from_url_or_path
 from tpv.rules import gateway
 
 
 def map_to_destination(tool, **kwd):
-    galaxy_app = mock_galaxy.App(job_conf=os.path.join(os.path.dirname(__file__), 'fixtures/job_conf.yml'))
-    job = mock_galaxy.Job()
-    user = mock_galaxy.User('gargravarr', 'fairycake@vortex.org')
-    gateway.ACTIVE_DESTINATION_MAPPERS = {}
-    return gateway.map_tool_to_destination(
-        galaxy_app, job, tool, user, **kwd
+    galaxy_app = mock_galaxy.App(
+        job_conf=os.path.join(os.path.dirname(__file__), "fixtures/job_conf.yml")
     )
+    job = mock_galaxy.Job()
+    user = mock_galaxy.User("gargravarr", "fairycake@vortex.org")
+    gateway.ACTIVE_DESTINATION_MAPPERS = {}
+    return gateway.map_tool_to_destination(galaxy_app, job, tool, user, **kwd)
+
 
 class TestMapperBasicFromPath(unittest.TestCase):
 
     @classmethod
     def _config_args(clazz, tpv_config_path=None):
-        tpv_config_path = tpv_config_path or os.path.join(os.path.dirname(__file__), 'fixtures/mapping-basic.yml')
+        tpv_config_path = tpv_config_path or os.path.join(
+            os.path.dirname(__file__), "fixtures/mapping-basic.yml"
+        )
         return dict(tpv_config_files=[tpv_config_path])
 
     def test_map_default_tool(self):
@@ -38,7 +40,9 @@ class TestMapperBasicFromPath(unittest.TestCase):
 
     def test_map_unschedulable_tool(self):
         tool = mock_galaxy.Tool("unschedulable_tool")
-        with self.assertRaisesRegex(JobMappingException, "No destinations are available to fulfill request"):
+        with self.assertRaisesRegex(
+            JobMappingException, "No destinations are available to fulfill request"
+        ):
             map_to_destination(tool, **self._config_args())
 
     def test_map_invalidly_tagged_tool(self):
@@ -85,6 +89,31 @@ class TestMapperBasicFromPath(unittest.TestCase):
 class TestMapperBasicFromYaml(TestMapperBasicFromPath):
     @classmethod
     def _config_args(clazz, tpv_config_path=None):
-        tpv_config_path = tpv_config_path or os.path.join(os.path.dirname(__file__), 'fixtures/mapping-basic.yml')
+        tpv_config_path = tpv_config_path or os.path.join(
+            os.path.dirname(__file__), "fixtures/mapping-basic.yml"
+        )
         python_dict = load_yaml_from_url_or_path(tpv_config_path)
         return dict(tpv_configs=[python_dict])
+
+
+class TestMapperConfigHandling(unittest.TestCase):
+
+    def test_map_with_both_path_and_yaml_config(self):
+        tool = mock_galaxy.Tool("my_concrete_tool")
+        tpv_config_path = os.path.join(
+            os.path.dirname(__file__), "fixtures/mapping-basic.yml"
+        )
+        python_dict = load_yaml_from_url_or_path(tpv_config_path)
+        with self.assertRaisesRegex(
+            ValueError, "Only one of tpv_configs or tpv_config_files can be specified"
+        ):
+            map_to_destination(
+                tool, tpv_config_files=tpv_config_path, tpv_configs=[python_dict]
+            )
+
+    def test_map_with_no_config(self):
+        tool = mock_galaxy.Tool("my_concrete_tool")
+        with self.assertRaisesRegex(
+            ValueError, "One of tpv_configs or tpv_config_files must be specified"
+        ):
+            map_to_destination(tool, tpv_config_files=None, tpv_configs=None)
